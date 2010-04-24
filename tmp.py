bb9a3b9a209185eeb96aca0876e9b5daa55d4f3d
root <root@ghunt-desktop.(none)>
2010-04-23 22:48:39 -0400

    removing the dead files

--- tmp.py
+++
@@ -1,4 +0,0 @@
-from Rpyc import *
-c = SocketConnection('localhost')
-print('instance %r'%c.modules.pydebug.pydebug_instance)
-print('pydebug home: %r'%c.modules.pydebug.pydebug_instance.pydebug_home)
